717f961a28b643045c0e1e54447c4d20af6ffbd6
SayfullayevNorbek <sayfullayevnorbek@gmail.com>
2023-10-11 01:33:42 +0500

    User and Role models created done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,18 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            RoleSeeder::class,
+            UserSeeder::class,
+
+        ]);
     }
 }
